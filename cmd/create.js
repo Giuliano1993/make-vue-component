0302efa1a156e423e5e72ca36708d2e683d954c7
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-10-15 10:52:41 +0200

    should work now

--- cmd/create.js
+++ cmd/create.js
@@ -9,7 +9,7 @@ const dir = path.join(path.dirname(process.argv[1]),'..');
 const configs = {
     INIT_PATH: dir,
     BASE_DIR: "./src",
-    STUBS_DIR: "./node_modules/make-vue-component/src/stubs",
+    STUBS_DIR: "./src/stubs",
     COMPONENT_FOLDER: "/components",
 }
 
