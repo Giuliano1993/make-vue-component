6279ba9129b0803ada02509942cbf1516e9b5bcd
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-11-06 16:46:08 +0100

    [ fix ]

--- cmd/create.js
+++ cmd/create.js
@@ -9,7 +9,7 @@ const dir = path.join(path.dirname(process.argv[1]),'..');
 const configs = {
     INIT_PATH: dir,
     BASE_DIR: "./src",
-    STUBS_DIR: "stubs",
+    STUBS_DIR: "stub",
     COMPONENT_FOLDER: "/components",
 }
 
